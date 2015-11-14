2179b3e0230351ad1a632f07f0b971fc65fd44de
Piran Tata <piran.tata@xero.com>
2015-11-14 13:56:22 +1300

    Code from session 3

---
+++ GameOfLife/GameOfLife.Tests/UnderPopulationRuleTest.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameOfLife.Tests
+{
+    public class UnderPopulationRuleTest
+    {
+        [Fact]
+        public void AssertCellDiesWhenLessThan2Neighbours()
+        {
+            var cell = new Cell(CellState.Alive);
+
+            var dead1 = new Cell(CellState.Dead);
+            var dead2 = new Cell(CellState.Dead);
+            var alive = new Cell(CellState.Alive);
+
+            var ruleState = new RuleState(cell, new[] { dead1, dead2, alive });
+
+
+            new UnderPopulationRule().Evaluate(ruleState);
+
+            Assert.Equal(CellState.Dead, cell.CellState);
+        }
+
+        [Fact]
+        public void CellStateIsAsIsWhenMoreThan2LivingNeighbours()
+        {
+            var cell = new Cell(CellState.Alive);
+
+            var dead1 = new Cell(CellState.Dead);
+            var alive1 = new Cell(CellState.Alive);
+            var alive2 = new Cell(CellState.Alive);
+
+            var ruleState = new RuleState(cell, new[] { dead1, alive1, alive2 });
+
+
+            new UnderPopulationRule().Evaluate(ruleState);
+
+            Assert.Equal(CellState.Alive, cell.CellState);
+        }
+    }
+
+
+    public class LivesToNextGenerstionRuleTest
+    {
+        [Fact]
+        public void CellStateLivesOnWithTwoOrThreeLivingNeighbours()
+        {
+            var cell = new Cell(CellState.Alive);
+
+            var dead1 = new Cell(CellState.Dead);
+            var alive1 = new Cell(CellState.Alive);
+            var alive2 = new Cell(CellState.Alive);
+
+            var ruleState = new RuleState(cell, new[] { dead1, alive1, alive2 });
+
+
+            new LivesToNextGenerstionRule().Evaluate(ruleState);
+
+            Assert.Equal(CellState.Alive, cell.CellState);
+        }
+
+
+        [Fact]
+        public void DeadCellStateIsStillDeadWithTwoOrThreeLivingNeighbours()
+        {
+            var cell = new Cell(CellState.Dead);
+
+            var dead1 = new Cell(CellState.Dead);
+            var alive1 = new Cell(CellState.Alive);
+            var alive2 = new Cell(CellState.Alive);
+
+            var ruleState = new RuleState(cell, new[] { dead1, alive1, alive2 });
+
+
+            new LivesToNextGenerstionRule().Evaluate(ruleState);
+
+            Assert.Equal(CellState.Dead, cell.CellState);
+        }
+
+
+        [Fact]
+        public void CellStateDiesWithLessThanTwoOrThreeLivingNeighbours()
+        {
+            var cell = new Cell(CellState.Alive);
+
+            var dead1 = new Cell(CellState.Dead);
+            var alive1 = new Cell(CellState.Alive);
+            var alive2 = new Cell(CellState.Alive);
+
+            var ruleState = new RuleState(cell, new[] { dead1, alive1, alive2 });
+
+
+            new LivesToNextGenerstionRule().Evaluate(ruleState);
+
+            Assert.Equal(CellState.Alive, cell.CellState);
+        }
+
+        [Fact]
+        public void LiveCellWithMoreThanThreeLivingNeighboursDies()
+        {
+            var cell = new Cell(CellState.Alive);
+
+            var dead1 = new Cell(CellState.Dead);
+            var alive1 = new Cell(CellState.Alive);
+            var alive2 = new Cell(CellState.Alive);
+            var alive3 = new Cell(CellState.Alive);
+            var alive4 = new Cell(CellState.Alive);
+
+            var ruleState = new RuleState(cell, new[] { dead1, alive1, alive2, alive3, alive4 });
+
+
+            new DeadTHroughOverpopulationRule().Evaluate(ruleState);
+
+            Assert.Equal(CellState.Dead, cell.CellState);
+        }
+
+
+        [Fact]
+        public void DeadCellWithMoreThanThreeLivingNeighboursStaysDead()
+        {
+            var cell = new Cell(CellState.Dead);
+
+            var dead1 = new Cell(CellState.Dead);
+            var alive1 = new Cell(CellState.Alive);
+            var alive2 = new Cell(CellState.Alive);
+            var alive3 = new Cell(CellState.Alive);
+            var alive4 = new Cell(CellState.Alive);
+
+            var ruleState = new RuleState(cell, new[] { dead1, alive1, alive2, alive3, alive4 });
+
+
+            new DeadTHroughOverpopulationRule().Evaluate(ruleState);
+
+            Assert.Equal(CellState.Dead, cell.CellState);
+        }
+
+        [Fact]
+        public void DeadCellWithThreeLivingNeighboursComesToLifeThroughReproduction()
+        {
+            var cell = new Cell(CellState.Dead);
+
+            var dead1 = new Cell(CellState.Dead);
+            var alive1 = new Cell(CellState.Alive);
+            var alive2 = new Cell(CellState.Alive);
+            var alive3 = new Cell(CellState.Alive);
+            //var alive4 = new Cell(CellState.Alive);
+
+            var ruleState = new RuleState(cell, new[] { dead1, alive1, alive2, alive3});
+
+
+            new ReproductionRule().Evaluate(ruleState);
+
+            Assert.Equal(CellState.Alive, cell.CellState);
+        }
+    }
+}
--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -12,4 +12,121 @@ namespace GameOfLife
         {
         }
     }
+
+
+    public class Cell
+    {
+        public CellState CellState { get; set; }
+
+        public Cell(CellState cellState)
+        {
+            CellState = cellState;
+        }
+    }
+
+    public enum CellState
+    {
+        Dead,
+        Alive
+    }
+
+    public class UnderPopulationRule
+    {
+        public void Evaluate(RuleState state)
+        {
+            //Do evalution
+            var countOfLivingNeighbours = state.Neighbours.Count(n => n.CellState == CellState.Alive);
+
+            //Write out the result to the state
+            if (countOfLivingNeighbours < 2)
+            {
+                state.Cell.CellState = CellState.Dead;
+            }
+            //else
+            //{
+            //    state.Result = state.Cell.CellState;
+            //}
+        }
+    }
+
+    public class RuleState
+    {
+        public Cell Cell { get; set; }
+        public IEnumerable<Cell> Neighbours { get; set; }
+        
+
+        public RuleState(Cell cell, IEnumerable<Cell> neighbours)
+        {
+            Cell = cell;
+            Neighbours = neighbours;
+        }
+    }
+
+    public class LivesToNextGenerstionRule
+    {
+        public void Evaluate(RuleState state)
+        {
+            //Do evalution
+            var countOfLivingNeighbours = state.Neighbours.Count(n => n.CellState == CellState.Alive);
+
+            //Write out the result to the state
+            if (countOfLivingNeighbours == 2 || countOfLivingNeighbours == 3)
+            {
+                state.Cell.CellState = state.Cell.CellState;
+            }
+            //else
+            //{
+            //    state.Result = state.Cell.CellState;
+            //}
+        }
+    }
+
+    public class DeadTHroughOverpopulationRule
+    {
+        public void Evaluate(RuleState state)
+        {
+            //Do evalution
+            var countOfLivingNeighbours = state.Neighbours.Count(n => n.CellState == CellState.Alive);
+
+            //Write out the result to the state
+            if (countOfLivingNeighbours > 3)
+            {
+                state.Cell.CellState = CellState.Dead;
+            }
+            //else
+            //{
+            //    state.Result = state.Cell.CellState;
+            //}
+        }
+    }
+
+    public class ReproductionRule
+    {
+        public void Evaluate(RuleState state)
+        {
+            //Do evalution
+            var countOfLivingNeighbours = state.Neighbours.Count(n => n.CellState == CellState.Alive);
+
+            //Write out the result to the state
+            if (countOfLivingNeighbours == 3)
+            {
+                state.Cell.CellState = CellState.Alive;
+            }
+            //else
+            //{
+            //    state.Result = state.Cell.CellState;
+            //}
+        }
+    }
+
+    public class CellStateCalculator
+    {
+        public void CalculateCellState(RuleState state)
+        {
+            new UnderPopulationRule().Evaluate(state);
+            new LivesToNextGenerstionRule().Evaluate(state);
+            new DeadTHroughOverpopulationRule().Evaluate(state);
+            new ReproductionRule().Evaluate(state);
+        }
+    }
 }
